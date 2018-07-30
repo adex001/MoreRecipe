21b375572982257a9a24bdc79c4f1c9b3d1da60b
Marcus <marcus2cu@gmail.com>
2018-07-30 14:31:25 +0100

    update authForm with switching conditions

--- script.js/authForm.js
+++ script.js/authForm.js
@@ -1,6 +1,19 @@
-const switchAuthForm = () => {
+const signinForm = document.querySelector('#signinForm');
+const signupForm = document.querySelector('#signupForm');
+
+const switchAuthForm = (event) => {
     const selectAuthDivs = document.querySelectorAll('.userForm');
-    selectAuthDivs[0].style.display = 'block';   
+    selectAuthDivs[0].style.display = 'block';
+    if (event.target.id = 'signinForm') {
+        selectAuthDivs[1].style.display = 'block';
+        selectAuthDivs[0].style.display = 'none'
+    }
+    else{
+        selectAuthDivs[1].style.display = 'none';
+        selectAuthDivs[0].style.display = 'block'
+    }
+    signinForm.addEventListener('click', switchAuthForm);
+    signupForm.addEventListener('click', switchAuthForm);
 }
 
 window.onload = switchAuthForm();
